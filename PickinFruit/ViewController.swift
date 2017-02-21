16dd634418fc4060325433090f6220519920291a
edmund-h <447ght@gmail.com>
2017-02-21 14:39:23 -0500

    will try advanced fns later

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var spinButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var fruitPicker: UIPickerView!
     
     var fruitsArray = ["🍎", "🍊", "🍌", "🍐", "🍇", "🍉", "🍓", "🍒", "🍍"]
     
@@ -23,9 +24,34 @@ class ViewController: UIViewController {
     }
     
     @IBAction func spinButtonTapped(_ sender: UIButton) {
+        let myFruit1 = Int(arc4random_uniform(UInt32(fruitsArray.count)))
+        let myFruit2 = Int(arc4random_uniform(UInt32(fruitsArray.count)))
+        let myFruit3 = Int(arc4random_uniform(UInt32(fruitsArray.count)))
+        fruitPicker.selectRow(myFruit1, inComponent: 0, animated: true)
+        fruitPicker.selectRow(myFruit2, inComponent: 1, animated: true)
+        fruitPicker.selectRow(myFruit3, inComponent: 2, animated: true)
+        let fruitAt1 = fruitPicker.selectedRow(inComponent: 0)
+        let fruitAt2 = fruitPicker.selectedRow(inComponent: 1)
+        let fruitAt3 = fruitPicker.selectedRow(inComponent: 2)
+        if fruitsArray[fruitAt1] == fruitsArray[fruitAt2]
+            && fruitsArray[fruitAt1] == fruitsArray[fruitAt3]{
+            resultLabel.text = "WINNER!"
+        }else{
+            resultLabel.text = "TRY AGAIN"
+        }
         
-        
-        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return fruitsArray[row]
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return fruitsArray.count
     }
 
 }
