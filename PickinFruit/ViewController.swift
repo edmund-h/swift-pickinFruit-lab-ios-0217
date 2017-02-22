2b1477d4d1453f57382c5f0f5f01588e621b25ea
edmund-h <447ght@gmail.com>
2017-02-21 21:59:50 -0500

    did one of the advanced fns

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -24,17 +24,17 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
     
     @IBAction func spinButtonTapped(_ sender: UIButton) {
-        let myFruit1 = Int(arc4random_uniform(UInt32(fruitsArray.count)))
-        let myFruit2 = Int(arc4random_uniform(UInt32(fruitsArray.count)))
-        let myFruit3 = Int(arc4random_uniform(UInt32(fruitsArray.count)))
+        let myFruit1 = Int(arc4random_uniform(UInt32(fruitPicker.numberOfRows(inComponent: 0))))
+        let myFruit2 = Int(arc4random_uniform(UInt32(fruitPicker.numberOfRows(inComponent: 1))))
+        let myFruit3 = Int(arc4random_uniform(UInt32(fruitPicker.numberOfRows(inComponent: 2))))
         fruitPicker.selectRow(myFruit1, inComponent: 0, animated: true)
         fruitPicker.selectRow(myFruit2, inComponent: 1, animated: true)
         fruitPicker.selectRow(myFruit3, inComponent: 2, animated: true)
         let fruitAt1 = fruitPicker.selectedRow(inComponent: 0)
         let fruitAt2 = fruitPicker.selectedRow(inComponent: 1)
         let fruitAt3 = fruitPicker.selectedRow(inComponent: 2)
-        if fruitsArray[fruitAt1] == fruitsArray[fruitAt2]
-            && fruitsArray[fruitAt1] == fruitsArray[fruitAt3]{
+        if fruitsArray[fruitAt1 % fruitsArray.count] == fruitsArray[fruitAt2 % fruitsArray.count]
+            && fruitsArray[fruitAt1 % fruitsArray.count] == fruitsArray[fruitAt3 % fruitsArray.count]{
             resultLabel.text = "WINNER!"
         }else{
             resultLabel.text = "TRY AGAIN"
@@ -43,7 +43,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return fruitsArray[row]
+        return fruitsArray[row % fruitsArray.count]
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -51,7 +51,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return fruitsArray.count
+        return fruitsArray.count * 1000
     }
 
 }
